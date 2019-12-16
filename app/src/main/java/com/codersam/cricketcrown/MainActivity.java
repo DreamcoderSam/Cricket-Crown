b8287c52c34e8774b722fe042daed4aa7d3e5a05
DreamedSam <samratcse08@gmail.com>
2019-12-16 16:07:16 +0600

    Count down removed

--- app/src/main/java/com/codersam/cricketcrown/MainActivity.java
+++ app/src/main/java/com/codersam/cricketcrown/MainActivity.java
@@ -27,11 +27,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtDay = (TextView) findViewById(R.id.days);
-        txtHour = (TextView) findViewById(R.id.hours);
-        txtMinute = (TextView) findViewById(R.id.minutes);
-        txtSecond = (TextView) findViewById(R.id.seconds);
-        countDownStart();
+//        txtDay = (TextView) findViewById(R.id.days);
+//        txtHour = (TextView) findViewById(R.id.hours);
+//        txtMinute = (TextView) findViewById(R.id.minutes);
+//        txtSecond = (TextView) findViewById(R.id.seconds);
+//        countDownStart();
 
         //LinearLayout Starts
         fixtureLayout = findViewById(R.id.fixture_layout);
@@ -72,51 +72,51 @@ public class MainActivity extends AppCompatActivity {
 
     }
     // CountDown Started
-    public void countDownStart() {
-        handler = new Handler();
-        runnable = new Runnable() {
-            @Override
-            public void run() {
-                handler.postDelayed(this, 1000);
-                try {
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-                    // Please here set your event date//YYYY-MM-DD
-                    Date futureDate = dateFormat.parse("2019-5-30");
-                    Date currentDate = new Date();
-                    if (!currentDate.after(futureDate)) {
-                        long diff = futureDate.getTime()
-                                - currentDate.getTime();
-                        long days = diff / (24 * 60 * 60 * 1000);
-                        diff -= days * (24 * 60 * 60 * 1000);
-                        long hours = diff / (60 * 60 * 1000);
-                        diff -= hours * (60 * 60 * 1000);
-                        long minutes = diff / (60 * 1000);
-                        diff -= minutes * (60 * 1000);
-                        long seconds = diff / 1000;
-                        txtDay.setText("" + String.format("%02d", days));
-                        txtHour.setText("" + String.format("%02d", hours));
-                        txtMinute.setText(""
-                                + String.format("%02d", minutes));
-                        txtSecond.setText(""
-                                + String.format("%02d", seconds));
-                    } else {
-                        textViewGone();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        handler.postDelayed(runnable, 1 * 1000);
-    }
-
-    public void textViewGone() {
-        findViewById(R.id.countDownDay).setVisibility(View.GONE);
-        findViewById(R.id.countDownHour).setVisibility(View.GONE);
-        findViewById(R.id.countDownMinute).setVisibility(View.GONE);
-        findViewById(R.id.countDownSeconds).setVisibility(View.GONE);
-        findViewById(R.id.countDownMain).setVisibility(View.GONE);
-        findViewById(R.id.countDownText).setVisibility(View.GONE);
-    }
-    //CountDown End
+//    public void countDownStart() {
+//        handler = new Handler();
+//        runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//                handler.postDelayed(this, 1000);
+//                try {
+//                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+//                    // Please here set your event date//YYYY-MM-DD
+//                    Date futureDate = dateFormat.parse("2019-5-30");
+//                    Date currentDate = new Date();
+//                    if (!currentDate.after(futureDate)) {
+//                        long diff = futureDate.getTime()
+//                                - currentDate.getTime();
+//                        long days = diff / (24 * 60 * 60 * 1000);
+//                        diff -= days * (24 * 60 * 60 * 1000);
+//                        long hours = diff / (60 * 60 * 1000);
+//                        diff -= hours * (60 * 60 * 1000);
+//                        long minutes = diff / (60 * 1000);
+//                        diff -= minutes * (60 * 1000);
+//                        long seconds = diff / 1000;
+//                        txtDay.setText("" + String.format("%02d", days));
+//                        txtHour.setText("" + String.format("%02d", hours));
+//                        txtMinute.setText(""
+//                                + String.format("%02d", minutes));
+//                        txtSecond.setText(""
+//                                + String.format("%02d", seconds));
+//                    } else {
+//                        textViewGone();
+//                    }
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        };
+//        handler.postDelayed(runnable, 1 * 1000);
+//    }
+//
+//    public void textViewGone() {
+//        findViewById(R.id.countDownDay).setVisibility(View.GONE);
+//        findViewById(R.id.countDownHour).setVisibility(View.GONE);
+//        findViewById(R.id.countDownMinute).setVisibility(View.GONE);
+//        findViewById(R.id.countDownSeconds).setVisibility(View.GONE);
+//        findViewById(R.id.countDownMain).setVisibility(View.GONE);
+//        findViewById(R.id.countDownText).setVisibility(View.GONE);
+//    }
+//    //CountDown End
 }
